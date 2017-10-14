593c3b0c4eabb0759656aba298791a052288e2cc
Xion <karol.kuczmarski@gmail.com>
2017-10-14 15:05:09 +0100

    Show one page of newest posts

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Default
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import System.IO
+
+import Reddit
+import Reddit.Types.Listing
+import Reddit.Types.Post
+
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  mapM_ setupIO [stdout, stdin]
+  void $ runRedditAnon $ do
+    Listing _ _ posts <- getPosts' def New Nothing
+    forM_ posts $ \post -> do
+      liftIO $ Text.putStrLn $ formatPost post
+  where
+  setupIO :: Handle -> IO ()
+  setupIO handle = do
+    hSetBuffering handle NoBuffering
+    hSetEncoding handle utf8
+
+formatPost :: Post -> Text
+formatPost post = "[" <> tshow (score post) <> "] " <> title post
+                  <> " (" <> subreddit' post <> ")"
+  where
+  subreddit' post = let R name = subreddit post in "/r/" <> name
+
+tshow :: Show a => a -> Text
+tshow = Text.pack . show
