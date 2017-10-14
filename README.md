eba6966be09d1a5fff96003eb17a411c7e2a4f3f
Xion <karol.kuczmarski@gmail.com>
2017-10-15 00:51:54 +0100

    Command line arguments

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # spiteful
 
 TBD
+
+## Development notes
+
+To install the ICU library on Windows (necessary for text-icu dependency),
+use Stack's msys & Pacman:
+
+    stack exec -- pacman -Sy mingw64/mingw-w64-x86_64-icu
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>), liftA2)
 import Control.Concurrent (threadDelay)
 import Control.Monad
 import Control.Monad.IO.Class
@@ -13,7 +14,6 @@ import Data.Either.Combinators (whenLeft)
 import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
 import Data.Text (Text)
-import Data.Version (showVersion)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import Data.Text.Format
@@ -22,23 +22,55 @@ import Data.Text.ICU.Char
 import Data.Text.ICU.Normalize
 import qualified Data.Text.IO as Text
 import Data.Text.Lazy (toStrict)
+import Data.Version (showVersion)
 import GHC.Generics (Generic)
-import System.IO
-
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.HTTP.Client.TLS as TLS
+import Options.Applicative
 import Pipes
 import qualified Pipes.Prelude as P
 import Reddit hiding (Options)
 import Reddit.Types.Listing
 import Reddit.Types.Post
+import System.IO
 
 import Paths_spiteful (version)
 
 
 defaultUserAgent :: Text
-defaultUserAgent = "spiteful-bot " <> Text.pack (showVersion version)
+defaultUserAgent = "spiteful-bot " <> botVersion
 
+botVersion :: Text
+botVersion = Text.pack $ showVersion version
+
+
+main :: IO ()
+main = do
+  opts <- execParser commandLine
+
+  logAt Info $ "spiteful-bot v" <> botVersion
+  forM_ [stdout, stdin] $ \handle -> do
+    hSetBuffering handle NoBuffering
+    hSetEncoding handle utf8
+
+  logAt Debug $ "Command line options: " <> (tshow opts)
+
+  TLS.setGlobalManager =<< TLS.newTlsManager
+
+  result <- runEffect $ fetchPosts opts New
+                      >-> P.filter isDontUpvotePost
+                      >-> P.map formatPost
+                      >-> P.print
+  whenLeft result $ \err -> Text.hPutStrLn stderr (tshow err)
+
+formatPost :: Post -> Text
+formatPost post = "[" <> tshow (score post) <> "] " <> title post
+                  <> " (" <> subreddit' post <> ")"
+  where
+  subreddit' post = let R name = subreddit post in "/r/" <> name
+
+
+-- Program options & command line
 
 data Options = Options
   { optCredentials :: Maybe (Text, Text)
@@ -56,27 +88,40 @@ toRedditOptions Options{..} = RedditOptions
   , customUserAgent = Just $ Text.encodeUtf8 $ fromMaybe defaultUserAgent optUserAgent
   }
 
+commandLine :: ParserInfo Options
+commandLine = info (options <**> helper)
+    ( fullDesc
+    <> progDesc "Run the Spiteful reddit bot"
+    <> header "Spiteful reddit bot"
+    <> failureCode 2
+    )
 
-main :: IO ()
-main = do
-  forM_ [stdout, stdin] $ \handle -> do
-    hSetBuffering handle NoBuffering
-    hSetEncoding handle utf8
-
-  TLS.setGlobalManager =<< TLS.newTlsManager
-
-  -- TODO: read options from the command line
-  result <- runEffect $ fetchPosts def New
-                      >-> P.filter isDontUpvotePost
-                      >-> P.map formatPost
-                      >-> P.print
-  whenLeft result $ \err -> Text.hPutStrLn stderr (tshow err)
-
-formatPost :: Post -> Text
-formatPost post = "[" <> tshow (score post) <> "] " <> title post
-                  <> " (" <> subreddit' post <> ")"
+options :: Parser Options
+options = do
+  username <- optional $ option str
+    ( long "user" <> short 'u' <> metavar "USERNAME"
+    <> help "Reddit username to use for the bot"
+    )
+  password <- optional $ option str
+    ( long "password" <> short 'p' <> metavar "PASSWORD"
+    <> help "Password to the bot's Reddit account"
+    )
+  userAgent <- optional $ option str
+    ( long "user-agent" <> short 'A'
+    <> metavar "USER-AGENT"
+    <> help "Custom value for the User-Agent header sent with all requests"
+    <> hidden
+    )
+  subreddit <- optional $ argument str
+    ( metavar "SUBREDDIT"
+    <> help "Subreddit to limit the bot to. By default, watches whole /new"
+    )
+  return def { optCredentials = liftA2 (,) username password
+             , optSubreddit = R . stripSlashR <$> subreddit
+             , optUserAgent = userAgent
+             }
   where
-  subreddit' post = let R name = subreddit post in "/r/" <> name
+  stripSlashR s = fromMaybe s $ Text.stripPrefix "/r/" s
 
 
 -- Fetching posts
@@ -90,6 +135,8 @@ fetchPosts options listingType = do
   httpManager <- liftIO $ HTTP.newManager TLS.tlsManagerSettings
   let redditOpts' = redditOpts { connectionManager = Just httpManager}
 
+  logAt Debug $ "Fetching posts from " <>
+    maybe "the entire Reddit" (("subreddit: " <>) . tshow) (optSubreddit options)
   doFetch httpManager redditOpts' listingType Nothing
   where
   doFetch :: MonadIO m
@@ -99,8 +146,8 @@ fetchPosts options listingType = do
     result <- liftIO $ runRedditWith rOpts $ do
       let postOpts = def { pagination = After <$> after, limit = Just 50 }
       Listing _ after' posts <- getPosts' postOpts lt (optSubreddit options)
-      logAt Debug "Received {} posts from the {} feed (after = {})"
-                  (length posts, tshow lt, tshow after)
+      logFmt Debug "Received {} posts from the {} feed (after = {})"
+                   (length posts, tshow lt, tshow after)
       return (posts, after')
     case result of
       Left err -> return $ Left err
@@ -124,11 +171,12 @@ isDontUpvotePost post = any (`Text.isInfixOf` title') phrases
 data LogLevel = Debug | Info | Warn | Error
                 deriving (Eq, Show)
 
-logAt :: (MonadIO m, Params ps) => LogLevel -> Format -> ps -> m ()
-logAt level fmt params =
-  let message = toStrict $ format fmt params
-  in liftIO $ Text.hPutStrLn stderr $
-    "[" <> Text.toUpper (tshow level) <> "] " <> message
+logFmt :: (MonadIO m, Params ps) => LogLevel -> Format -> ps -> m ()
+logFmt level fmt params = logAt level $ toStrict $ format fmt params
+
+logAt :: MonadIO m => LogLevel -> Text -> m ()
+logAt level msg = liftIO $ Text.hPutStrLn stderr $
+  "[" <> Text.toUpper (tshow level) <> "] " <> msg
 
 
 -- Utilities
